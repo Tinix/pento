5dbc8833ad936d572d161a40ca8337b4bdc88c0f
Daniel Tinivella <dtinivella@gmail.com>
2024-02-10 13:28:30 -0300

    add LiveProducts to Pento

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -26,6 +26,13 @@ defmodule PentoWeb.Router do
 
     get "/", PageController, :home
     live "/guess", WrongLive
+
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
   end
 
   # Other scopes may use custom stacks.
